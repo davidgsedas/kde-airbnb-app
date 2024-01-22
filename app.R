f971be24148e0b3d59e277833b43615520048c09
davidgsedas <davidgsedas@gmail.com>
2024-01-22 17:59:31 +0100

    Interfaz completa

--- app.R
+++ app.R
@@ -10,198 +10,104 @@ lista_barrios <- unique(df_shiny$barrio)
 # Shiny
 # UI
 ui <- fluidPage(
-  titlePanel("AirBnb Apartment Dashboard"),
+  titlePanel("Airbnb Apartment Dashboard"),
   
   sidebarLayout(
     sidebarPanel(
-      selectInput("barrio",
-                  "Barrio",
-                  unique(df_shiny$barrio)),
       
-      sliderInput("latitud",
-                  "Latitud",
-                  value = c(41.35186, 41.45956),
-                  min = 41.35186,
-                  max = 41.45956,
-                  step = 0.001),
+      h1("Menú de filtros"),
       
-      sliderInput("longitud",
-                  "Longitud",
-                  value = c(41.35186, 41.45956),
-                  min = 41.35186,
-                  max = 41.45956,
-                  step = 0.001),
+      h2("Localización"),
       
-      selectInput("tipo_propiedad",
-                  "Tipo de propiedad",
-                  choices = c(1, 2)),
+      selectInput("barrio", "Barrio", choices = lista_barrios),
       
-      sliderInput("capacidad",
-                  "Capacidad de personas",
-                  value = c(min(df_shiny$capacidad), max(df_shiny$capacidad)),
-                  min = min(df_shiny$capacidad),
-                  max = max(df_shiny$capacidad),
-                  step = 1),
+      sliderInput("latitud", "Latitud", value = c(41.35186, 41.45956), min = 41.35186, max = 41.45956, step = 0.001),
       
-      sliderInput("banos",
-                  "Número de baños",
-                  value = c(min(df_shiny$banos), max(df_shiny$banos)),
-                  min = min(df_shiny$banos),
-                  max = max(df_shiny$banos),
-                  step = 0.5),
+      sliderInput("longitud", "Longitud", value = c(2.15899, 2.18336), min = 2.15899, max = 2.18336, step = 0.001),
       
-      selectInput("bano_compartido",
-                  "Baño compartido",
-                  unique(df_shiny$bano_compartido)),
+      br(),
       
-      sliderInput("dormitorios",
-                  "Número de dormitorios",
-                  value = c(min(df_shiny$dormitorios), max(df_shiny$dormitorios)),
-                  min = min(df_shiny$dormitorios),
-                  max = max(df_shiny$dormitorios),
-                  step = 1),
+      h2("Características de la propiedad"),
       
-      sliderInput("camas",
-                  "Número de camas",
-                  value = c(min(df_shiny$camas), max(df_shiny$camas)),
-                  min = min(df_shiny$camas),
-                  max = max(df_shiny$camas),
-                  step = 1),
+      selectInput("tipo_propiedad", "Tipo de propiedad", choices = unique(df_shiny$tipo_propiedad)),
       
-      sliderInput("precio",
-                  "Precio",
-                  value = c(min(df_shiny$precio), max(df_shiny$precio)),
-                  min = min(df_shiny$precio),
-                  max = max(df_shiny$precio),
-                  step = 1),
+      sliderInput("capacidad", "Capacidad de personas", value = c(min(df_shiny$capacidad), max(df_shiny$capacidad)), min = min(df_shiny$capacidad), max = max(df_shiny$capacidad), step = 1),
       
-      sliderInput("noches_minimas",
-                  "Noches mínimas",
-                  value = min(df_shiny$noches_minimas),
-                  min = min(df_shiny$noches_minimas),
-                  max = max(df_shiny$noches_minimas),
-                  step = 1),
+      sliderInput("banos", "Número de baños", value = c(min(df_shiny$banos), max(df_shiny$banos)), min = min(df_shiny$banos), max = max(df_shiny$banos), step = 0.5),
       
-      sliderInput("noches_maximas",
-                  "Noches mínimas",
-                  value = min(df_shiny$noches_minimas),
-                  min = min(df_shiny$noches_minimas),
-                  max = max(df_shiny$noches_minimas),
-                  step = 1),
+      selectInput("bano_compartido", "Baño compartido", choices = unique(df_shiny$bano_compartido)),
       
-      selectInput("disponible",
-                  "¿Está disponible?",
-                  unique(df_shiny$disponible)),
+      sliderInput("dormitorios", "Número de dormitorios", value = c(min(df_shiny$dormitorios), max(df_shiny$dormitorios)), min = min(df_shiny$dormitorios), max = max(df_shiny$dormitorios), step = 1),
       
-      numericInput("disponibilidad_30",
-                 "Mínimo de disponibilidad en los próximos 30 dias",
-                 value=0,
-                 min=0,
-                 max=30),
+      sliderInput("camas", "Número de camas", value = c(min(df_shiny$camas), max(df_shiny$camas)), min = min(df_shiny$camas), max = max(df_shiny$camas), step = 1),
       
-      numericInput("disponibilidad_60",
-                   "Mínimo de disponibilidad en los próximos 60 dias",
-                   value=0,
-                   min=0,
-                   max=60),
+      br(),
       
-      numericInput("disponibilidad_90",
-                   "Mínimo de disponibilidad en los próximos 90 dias",
-                   value=0,
-                   min=0,
-                   max=90),
+      h2("Condiciones de la reserva"),
       
+      sliderInput("precio", "Precio", value = c(min(df_shiny$precio), max(df_shiny$precio)), min = min(df_shiny$precio), max = max(df_shiny$precio), step = 1),
       
-      numericInput("disponibilidad_365",
-                   "Mínimo de disponibilidad en los próximos 365 dias",
-                   value=0,
-                   min=0,
-                   max=90),
+      sliderInput("noches_minimas", "Noches mínimas", value = min(df_shiny$noches_minimas), min = min(df_shiny$noches_minimas), max = max(df_shiny$noches_minimas), step = 1),
       
+      sliderInput("noches_maximas", "Noches máximas", value = min(df_shiny$noches_maximas), min = min(df_shiny$noches_maximas), max = max(df_shiny$noches_maximas), step = 1),
       
-      numericInput("n_reviews_u30d",
-                   "Mínimo de reviews en los últimos 30 días",
-                   value=0,
-                   min=0,
-                   max = max(df$number_of_reviews_l30d)),
+      br(),
       
-      sliderInput("noches_maximas",
-                  "Noches mínimas",
-                  value = min(df_shiny$noches_minimas),
-                  min = min(df_shiny$noches_minimas),
-                  max = max(df_shiny$noches_minimas),
-                  step = 1),
+      h2("Disponibilidad"),
       
-      sliderInput("puntuacion_reviews_general",
-                  "Puntuación reviews",
-                  value = min(df_shiny$puntuacion_reviews_general),
-                  min = min(df_shiny$puntuacion_reviews_general),
-                  max = max(df_shiny$puntuacion_reviews_general),
-                  step = 0.1),
+      selectInput("disponible", "¿Está disponible?", choices = unique(df_shiny$disponible)),
       
-      sliderInput("puntuacion_reviews_precision",
-                  "Puntuación reviews en precisión",
-                  value = min(df_shiny$puntuacion_reviews_precision),
-                  min = min(df_shiny$puntuacion_reviews_precision),
-                  max = max(df_shiny$puntuacion_reviews_precision),
-                  step = 0.1),
+      numericInput("disponibilidad_30", "Mínimo de disponibilidad en los próximos 30 dias", value=0, min=0, max=30),
       
-      sliderInput("puntuacion_reviews_limpieza",
-                  "Puntuación reviews en limpieza",
-                  value = min(df_shiny$puntuacion_reviews_limpieza),
-                  min = min(df_shiny$puntuacion_reviews_limpieza),
-                  max = max(df_shiny$puntuacion_reviews_limpieza),
-                  step = 0.1),
-        
-
-      sliderInput("puntuacion_reviews_checkin",
-                  "Puntuación reviews en check-in",
-                  value = min(df_shiny$puntuacion_reviews_checkin),
-                  min = min(df_shiny$puntuacion_reviews_checkin),
-                  max = max(df_shiny$puntuacion_reviews_checkin),
-                  step = 0.1),
+      numericInput("disponibilidad_60", "Mínimo de disponibilidad en los próximos 60 dias", value=0, min=0, max=60),
       
-      sliderInput("puntuacion_reviews_comunicacion",
-                  "Puntuación reviews en comunicación",
-                  value = min(df_shiny$puntuacion_reviews_comunicacion),
-                  min = min(df_shiny$puntuacion_reviews_comunicacion),
-                  max = max(df_shiny$puntuacion_reviews_comunicacion),
-                  step = 0.1),
+      numericInput("disponibilidad_90", "Mínimo de disponibilidad en los próximos 90 dias", value=0, min=0, max=90),
       
-      sliderInput("puntuacion_reviews_localizacion",
-                  "Puntuación reviews en localización",
-                  value = min(df_shiny$puntuacion_reviews_localizacion),
-                  min = min(df_shiny$puntuacion_reviews_localizacion),
-                  max = max(df_shiny$puntuacion_reviews_localizacion),
-                  step = 0.1),
+      numericInput("disponibilidad_365", "Mínimo de disponibilidad en los próximos 365 dias", value=0, min=0, max=365),
       
+      numericInput("n_reviews_u30d", "Mínimo de reviews en los últimos 30 días", value=0, min=0, max=max(df_shiny$number_of_reviews_l30d)),
       
-      sliderInput("puntuacion_reviews_valor",
-                  "Puntuación reviews en valor",
-                  value = min(df_shiny$puntuacion_reviews_valor),
-                  min = min(df_shiny$puntuacion_reviews_valor),
-                  max = max(df_shiny$puntuacion_reviews_valor),
-                  step = 0.1),
+      br(),
       
+      h2("Reseñas"),
       
-      # Añade tantos filtros como necesites
+      sliderInput("puntuacion_reviews_general", "Puntuación reviews", value = min(df_shiny$puntuacion_reviews_general), min = min(df_shiny$puntuacion_reviews_general), max = max(df_shiny$puntuacion_reviews_general), step = 0.1),
+      
+      sliderInput("puntuacion_reviews_precision", "Puntuación reviews en precisión", value = min(df_shiny$puntuacion_reviews_precision), min = min(df_shiny$puntuacion_reviews_precision), max = max(df_shiny$puntuacion_reviews_precision), step = 0.1),
+      
+      sliderInput("puntuacion_reviews_limpieza", "Puntuación reviews en limpieza", value = min(df_shiny$puntuacion_reviews_limpieza), min = min(df_shiny$puntuacion_reviews_limpieza), max = max(df_shiny$puntuacion_reviews_limpieza), step = 0.1),
+      
+      sliderInput("puntuacion_reviews_checkin", "Puntuación reviews en check-in", value = min(df_shiny$puntuacion_reviews_checkin), min = min(df_shiny$puntuacion_reviews_checkin), max = max(df_shiny$puntuacion_reviews_checkin), step = 0.1),
+      
+      sliderInput("puntuacion_reviews_comunicacion", "Puntuación reviews en comunicación", value = min(df_shiny$puntuacion_reviews_comunicacion), min = min(df_shiny$puntuacion_reviews_comunicacion), max = max(df_shiny$puntuacion_reviews_comunicacion), step = 0.1),
+      
+      sliderInput("puntuacion_reviews_localizacion", "Puntuación reviews en localización", value = min(df_shiny$puntuacion_reviews_localizacion), min = min(df_shiny$puntuacion_reviews_localizacion), max = max(df_shiny$puntuacion_reviews_localizacion), step = 0.1),
+      
+      sliderInput("puntuacion_reviews_valor", "Puntuación reviews en valor", value = min(df_shiny$puntuacion_reviews_valor), min = min(df_shiny$puntuacion_reviews_valor), max = max(df_shiny$puntuacion_reviews_valor), step = 0.1),
+      
     ),
+    
     mainPanel(
+      
+      h1("Tabla filtrada"),
+      
       DTOutput("tablaFiltrada")
     )
   )
 )
-)
 
 server <- function(input, output) {
-  # Supongamos que 'mi_dataframe' es tu dataframe original
-  df_shiny <- readRDS("clean_airbnb_data.rds")  
   # Crear un dataframe reactivo que se actualiza con los filtros
   dataframeFiltrado <- reactive({
     df_temp <- df_shiny
-    # Aplicar filtros
-    # Ejemplo: df_temp <- df_temp[df_temp$columna == input$filtro1, ]
-    # Repite el proceso para cada filtro
+    # Aplicar filtros según las entradas del usuario
+    # Por ejemplo:
+    # df_temp <- df_temp[df_temp$barrio == input$barrio, ]
+    # Repite el proceso para cada filtro. Por ejemplo:
+    # if (!is.null(input$tipo_propiedad)) {
+    #   df_temp <- df_temp[df_temp$tipo_propiedad == input$tipo_propiedad, ]
+    # }
+    # Continúa aplicando filtros de manera similar para los demás inputs
     df_temp
   })
   
@@ -211,6 +117,5 @@ server <- function(input, output) {
   })
 }
 
-
 shinyApp(ui = ui, server = server)
-
+                                                                                                                                                
