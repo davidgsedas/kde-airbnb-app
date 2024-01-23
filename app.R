c4f5f9f3bbc2eab53db6b6e4f704e2decfd526e5
davidgsedas <davidgsedas@gmail.com>
2024-01-23 02:27:04 +0100

    Implementación solución NA's , actualización IA y planteamiento server

--- app.R
+++ app.R
@@ -1,121 +1,125 @@
 library(shiny)
-library(DT)  # Para tablas interactivas
+library(DT)
 
-# Cargo el archivo RDS
+# Carga el archivo RDS
 df_shiny <- readRDS("clean_airbnb_data.rds")
 
-# Datos
-lista_barrios <- unique(df_shiny$barrio)
+# Ajustes para el filtrado
+df_shiny$barrio <- as.character(df_shiny$barrio)
+df_shiny$tipo_propiedad <- as.character(df_shiny$tipo_propiedad)
 
-# Shiny
 # UI
 ui <- fluidPage(
   titlePanel("Airbnb Apartment Dashboard"),
   
   sidebarLayout(
     sidebarPanel(
-      
       h1("Menú de filtros"),
       
+      br(),
+      
       h2("Localización"),
-      
-      selectInput("barrio", "Barrio", choices = lista_barrios),
-      
-      sliderInput("latitud", "Latitud", value = c(41.35186, 41.45956), min = 41.35186, max = 41.45956, step = 0.001),
-      
-      sliderInput("longitud", "Longitud", value = c(2.15899, 2.18336), min = 2.15899, max = 2.18336, step = 0.001),
+      selectInput("barrio", "Barrio", choices = c("Todos", unique(df_shiny$barrio))),
+      sliderInput("latitud", "Latitud", value = c(min(df_shiny$latitud, na.rm = TRUE), max(df_shiny$latitud, na.rm = TRUE)), min = min(df_shiny$latitud, na.rm = TRUE), max = max(df_shiny$latitud, na.rm = TRUE), step = 0.001),
+      sliderInput("longitud", "Longitud", value = c(min(df_shiny$longitud, na.rm = TRUE), max(df_shiny$longitud, na.rm = TRUE)), min = min(df_shiny$longitud, na.rm = TRUE), max = max(df_shiny$longitud, na.rm = TRUE), step = 0.001),
       
       br(),
       
       h2("Características de la propiedad"),
-      
-      selectInput("tipo_propiedad", "Tipo de propiedad", choices = unique(df_shiny$tipo_propiedad)),
-      
-      sliderInput("capacidad", "Capacidad de personas", value = c(min(df_shiny$capacidad), max(df_shiny$capacidad)), min = min(df_shiny$capacidad), max = max(df_shiny$capacidad), step = 1),
-      
-      sliderInput("banos", "Número de baños", value = c(min(df_shiny$banos), max(df_shiny$banos)), min = min(df_shiny$banos), max = max(df_shiny$banos), step = 0.5),
-      
-      selectInput("bano_compartido", "Baño compartido", choices = unique(df_shiny$bano_compartido)),
-      
-      sliderInput("dormitorios", "Número de dormitorios", value = c(min(df_shiny$dormitorios), max(df_shiny$dormitorios)), min = min(df_shiny$dormitorios), max = max(df_shiny$dormitorios), step = 1),
-      
-      sliderInput("camas", "Número de camas", value = c(min(df_shiny$camas), max(df_shiny$camas)), min = min(df_shiny$camas), max = max(df_shiny$camas), step = 1),
+      selectInput("tipo_propiedad", "Tipo de propiedad", choices = c("Todos", unique(df_shiny$tipo_propiedad))),
+      sliderInput("capacidad", "Capacidad de personas", value = c(min(df_shiny$capacidad, na.rm = TRUE), max(df_shiny$capacidad, na.rm = TRUE)), min = min(df_shiny$capacidad, na.rm = TRUE), max = max(df_shiny$capacidad, na.rm = TRUE), step = 1),
+      sliderInput("banos", "Número de baños", value = c(min(df_shiny$banos, na.rm = TRUE), max(df_shiny$banos, na.rm = TRUE)), min = min(df_shiny$banos, na.rm = TRUE), max = max(df_shiny$banos, na.rm = TRUE), step = 0.5),
+      selectInput("bano_compartido", "Baño compartido", choices = c("Todos", unique(df_shiny$bano_compartido))),
+      sliderInput("dormitorios", "Número de dormitorios", value = c(min(df_shiny$dormitorios, na.rm = TRUE), max(df_shiny$dormitorios, na.rm = TRUE)), min = min(df_shiny$dormitorios, na.rm = TRUE), max = max(df_shiny$dormitorios, na.rm = TRUE), step = 1),
+      sliderInput("camas", "Número de camas", value = c(min(df_shiny$camas, na.rm = TRUE), max(df_shiny$camas, na.rm = TRUE)), min = min(df_shiny$camas, na.rm = TRUE), max = max(df_shiny$camas, na.rm = TRUE), step = 1),
       
       br(),
       
       h2("Condiciones de la reserva"),
-      
-      sliderInput("precio", "Precio", value = c(min(df_shiny$precio), max(df_shiny$precio)), min = min(df_shiny$precio), max = max(df_shiny$precio), step = 1),
-      
-      sliderInput("noches_minimas", "Noches mínimas", value = min(df_shiny$noches_minimas), min = min(df_shiny$noches_minimas), max = max(df_shiny$noches_minimas), step = 1),
-      
-      sliderInput("noches_maximas", "Noches máximas", value = min(df_shiny$noches_maximas), min = min(df_shiny$noches_maximas), max = max(df_shiny$noches_maximas), step = 1),
+      sliderInput("precio", "Precio", value = c(min(df_shiny$precio, na.rm = TRUE), max(df_shiny$precio, na.rm = TRUE)), min = min(df_shiny$precio, na.rm = TRUE), max = max(df_shiny$precio, na.rm = TRUE), step = 1),
+      sliderInput("noches_minimas", "Noches mínimas", value = c(min(df_shiny$noches_minimas, na.rm = TRUE), max(df_shiny$noches_minimas, na.rm = TRUE)), min = min(df_shiny$noches_minimas, na.rm = TRUE), max = max(df_shiny$noches_minimas, na.rm = TRUE), step = 1),
+      sliderInput("noches_maximas", "Noches máximas", value = c(min(df_shiny$noches_maximas, na.rm = TRUE), max(df_shiny$noches_maximas, na.rm = TRUE)), min = min(df_shiny$noches_maximas, na.rm = TRUE), max = max(df_shiny$noches_maximas, na.rm = TRUE), step = 1),
       
       br(),
       
       h2("Disponibilidad"),
-      
-      selectInput("disponible", "¿Está disponible?", choices = unique(df_shiny$disponible)),
-      
-      numericInput("disponibilidad_30", "Mínimo de disponibilidad en los próximos 30 dias", value=0, min=0, max=30),
-      
-      numericInput("disponibilidad_60", "Mínimo de disponibilidad en los próximos 60 dias", value=0, min=0, max=60),
-      
-      numericInput("disponibilidad_90", "Mínimo de disponibilidad en los próximos 90 dias", value=0, min=0, max=90),
-      
-      numericInput("disponibilidad_365", "Mínimo de disponibilidad en los próximos 365 dias", value=0, min=0, max=365),
-      
-      numericInput("n_reviews_u30d", "Mínimo de reviews en los últimos 30 días", value=0, min=0, max=max(df_shiny$number_of_reviews_l30d)),
+      selectInput("disponible", "¿Está disponible?", choices = c("Todos", unique(df_shiny$disponible))),
+      numericInput("disponibilidad_30", "Mínimo de disponibilidad en los próximos 30 días", value = 0, min = 0, max = 30),
+      numericInput("disponibilidad_60", "Mínimo de disponibilidad en los próximos 60 días", value = 0, min = 0, max = 60),
+      numericInput("disponibilidad_90", "Mínimo de disponibilidad en los próximos 90 días", value = 0, min = 0, max = 90),
+      numericInput("disponibilidad_365", "Mínimo de disponibilidad en los próximos 365 días", value = 0, min = 0, max = 365),
       
       br(),
       
       h2("Reseñas"),
-      
-      sliderInput("puntuacion_reviews_general", "Puntuación reviews", value = min(df_shiny$puntuacion_reviews_general), min = min(df_shiny$puntuacion_reviews_general), max = max(df_shiny$puntuacion_reviews_general), step = 0.1),
-      
-      sliderInput("puntuacion_reviews_precision", "Puntuación reviews en precisión", value = min(df_shiny$puntuacion_reviews_precision), min = min(df_shiny$puntuacion_reviews_precision), max = max(df_shiny$puntuacion_reviews_precision), step = 0.1),
-      
-      sliderInput("puntuacion_reviews_limpieza", "Puntuación reviews en limpieza", value = min(df_shiny$puntuacion_reviews_limpieza), min = min(df_shiny$puntuacion_reviews_limpieza), max = max(df_shiny$puntuacion_reviews_limpieza), step = 0.1),
-      
-      sliderInput("puntuacion_reviews_checkin", "Puntuación reviews en check-in", value = min(df_shiny$puntuacion_reviews_checkin), min = min(df_shiny$puntuacion_reviews_checkin), max = max(df_shiny$puntuacion_reviews_checkin), step = 0.1),
-      
-      sliderInput("puntuacion_reviews_comunicacion", "Puntuación reviews en comunicación", value = min(df_shiny$puntuacion_reviews_comunicacion), min = min(df_shiny$puntuacion_reviews_comunicacion), max = max(df_shiny$puntuacion_reviews_comunicacion), step = 0.1),
-      
-      sliderInput("puntuacion_reviews_localizacion", "Puntuación reviews en localización", value = min(df_shiny$puntuacion_reviews_localizacion), min = min(df_shiny$puntuacion_reviews_localizacion), max = max(df_shiny$puntuacion_reviews_localizacion), step = 0.1),
-      
-      sliderInput("puntuacion_reviews_valor", "Puntuación reviews en valor", value = min(df_shiny$puntuacion_reviews_valor), min = min(df_shiny$puntuacion_reviews_valor), max = max(df_shiny$puntuacion_reviews_valor), step = 0.1),
+      sliderInput("puntuacion_reviews_general", "Puntuación general de reseñas", value = c(min(df_shiny$puntuacion_reviews_general, na.rm = TRUE), max(df_shiny$puntuacion_reviews_general, na.rm = TRUE)), min = min(df_shiny$puntuacion_reviews_general, na.rm = TRUE), max = max(df_shiny$puntuacion_reviews_general, na.rm = TRUE), step = 0.1),
+      sliderInput("puntuacion_reviews_precision", "Puntuación de reseñas en precisión", value = c(min(df_shiny$puntuacion_reviews_precision, na.rm = TRUE), max(df_shiny$puntuacion_reviews_precision, na.rm = TRUE)), min = min(df_shiny$puntuacion_reviews_precision, na.rm = TRUE), max = max(df_shiny$puntuacion_reviews_precision, na.rm = TRUE), step = 0.1),
+      sliderInput("puntuacion_reviews_limpieza", "Puntuación de reseñas en limpieza", value = c(min(df_shiny$puntuacion_reviews_limpieza, na.rm = TRUE), max(df_shiny$puntuacion_reviews_limpieza, na.rm = TRUE)), min = min(df_shiny$puntuacion_reviews_limpieza, na.rm = TRUE), max = max(df_shiny$puntuacion_reviews_limpieza, na.rm = TRUE), step = 0.1),
+      sliderInput("puntuacion_reviews_checkin", "Puntuación de reseñas en check-in", value = c(min(df_shiny$puntuacion_reviews_checkin, na.rm = TRUE), max(df_shiny$puntuacion_reviews_checkin, na.rm = TRUE)), min = min(df_shiny$puntuacion_reviews_checkin, na.rm = TRUE), max = max(df_shiny$puntuacion_reviews_checkin, na.rm = TRUE), step = 0.1),
+      sliderInput("puntuacion_reviews_comunicacion", "Puntuación de reseñas en comunicación", value = c(min(df_shiny$puntuacion_reviews_comunicacion, na.rm = TRUE), max(df_shiny$puntuacion_reviews_comunicacion, na.rm = TRUE)), min = min(df_shiny$puntuacion_reviews_comunicacion, na.rm = TRUE), max = max(df_shiny$puntuacion_reviews_comunicacion, na.rm = TRUE), step = 0.1),
+      sliderInput("puntuacion_reviews_localizacion", "Puntuación de reseñas en localización", value = c(min(df_shiny$puntuacion_reviews_localizacion, na.rm = TRUE), max(df_shiny$puntuacion_reviews_localizacion, na.rm = TRUE)), min = min(df_shiny$puntuacion_reviews_localizacion, na.rm = TRUE), max = max(df_shiny$puntuacion_reviews_localizacion, na.rm = TRUE), step = 0.1),
+      sliderInput("puntuacion_reviews_valor", "Puntuación de reseñas en valor", value = c(min(df_shiny$puntuacion_reviews_valor, na.rm = TRUE), max(df_shiny$puntuacion_reviews_valor, na.rm = TRUE)), min = min(df_shiny$puntuacion_reviews_valor, na.rm = TRUE), max = max(df_shiny$puntuacion_reviews_valor, na.rm = TRUE), step = 0.1)
       
     ),
-    
     mainPanel(
-      
       h1("Tabla filtrada"),
-      
       DTOutput("tablaFiltrada")
     )
   )
 )
 
 server <- function(input, output) {
-  # Crear un dataframe reactivo que se actualiza con los filtros
-  dataframeFiltrado <- reactive({
-    df_temp <- df_shiny
-    # Aplicar filtros según las entradas del usuario
-    # Por ejemplo:
-    # df_temp <- df_temp[df_temp$barrio == input$barrio, ]
-    # Repite el proceso para cada filtro. Por ejemplo:
-    # if (!is.null(input$tipo_propiedad)) {
-    #   df_temp <- df_temp[df_temp$tipo_propiedad == input$tipo_propiedad, ]
-    # }
-    # Continúa aplicando filtros de manera similar para los demás inputs
-    df_temp
-  })
-  
-  # Renderizar la tabla
   output$tablaFiltrada <- renderDT({
-    datatable(dataframeFiltrado())
+    filtered_data <- df_shiny
+    
+    # # Filtrado por barrio
+    if(input$barrio != "Todos") {
+       filtered_data <- filtered_data[filtered_data$barrio == input$barrio, ]
+     }
+    
+    # Filtrado por tipo de propiedad
+    if(input$tipo_propiedad != "Todos") {
+      filtered_data <- filtered_data[filtered_data$tipo_propiedad == input$tipo_propiedad, ]
+    }
+    
+    # Filtrado por disponibilidad
+    if(input$disponible != "Todos") {
+      disponibilidad <- ifelse(input$disponible == TRUE, FALSE)
+      filtered_data <- filtered_data[filtered_data$disponible == disponibilidad, ]
+    }
+    
+    # Filtrado por sliders y numericInputs
+
+    filtered_data <- filtered_data$latitud >= input$latitud[1] & filtered_data$latitud <= input$latitud[2] 
+      filtered_data <- filtered_data$longitud >= input$longitud[1] & filtered_data$longitud <= input$longitud[2] 
+      filtered_data <- filtered_data$capacidad >= input$capacidad[1] & filtered_data$capacidad <= input$capacidad[2] 
+      filtered_data <- filtered_data$banos >= input$banos[1] & filtered_data$banos <= input$banos[2] 
+      filtered_data <- filtered_data$dormitorios >= input$dormitorios[1] & filtered_data$dormitorios <= input$dormitorios[2] 
+      filtered_data <- filtered_data$camas >= input$camas[1] & filtered_data$camas <= input$camas[2] 
+      filtered_data <- filtered_data$precio >= input$precio[1] & filtered_data$precio <= input$precio[2] 
+      filtered_data <- filtered_data$noches_minimas >= input$noches_minimas[1] & filtered_data$noches_minimas <= input$noches_minimas[2] 
+      filtered_data <- filtered_data$noches_maximas >= input$noches_maximas[1] & filtered_data$noches_maximas <= input$noches_maximas[2] 
+      filtered_data <- filtered_data$disponibilidad_30 >= input$disponibilidad_30 
+      filtered_data <- filtered_data$disponibilidad_60 >= input$disponibilidad_60 
+      filtered_data <- filtered_data$disponibilidad_90 >= input$disponibilidad_90 
+      filtered_data <- filtered_data$disponibilidad_365 >= input$disponibilidad_365 
+      filtered_data <- filtered_data$puntuacion_reviews_general >= input$puntuacion_reviews_general[1] & filtered_data$puntuacion_reviews_general <= input$puntuacion_reviews_general[2] 
+      filtered_data <- filtered_data$puntuacion_reviews_precision >= input$puntuacion_reviews_precision[1] & filtered_data$puntuacion_reviews_precision <= input$puntuacion_reviews_precision[2] 
+      filtered_data <- filtered_data$puntuacion_reviews_limpieza >= input$puntuacion_reviews_limpieza[1] & filtered_data$puntuacion_reviews_limpieza <= input$puntuacion_reviews_limpieza[2] 
+      filtered_data <- filtered_data$puntuacion_reviews_checkin >= input$puntuacion_reviews_checkin[1] & filtered_data$puntuacion_reviews_checkin <= input$puntuacion_reviews_checkin[2] 
+      filtered_data <- filtered_data$puntuacion_reviews_comunicacion >= input$puntuacion_reviews_comunicacion[1] & filtered_data$puntuacion_reviews_comunicacion <= input$puntuacion_reviews_comunicacion[2] 
+      filtered_data <- filtered_data$puntuacion_reviews_localizacion >= input$puntuacion_reviews_localizacion[1] & filtered_data$puntuacion_reviews_localizacion <= input$puntuacion_reviews_localizacion[2] 
+      filtered_data <- filtered_data$puntuacion_reviews_valor >= input$puntuacion_reviews_valor[1] & filtered_data$puntuacion_reviews_valor <= input$puntuacion_reviews_valor[2]
+     
+     datatable(filtered_data)
   })
 }
 
+
+
+# Ejecuta la aplicación
 shinyApp(ui = ui, server = server)
-                                                                                                                                                
+
+
+
+
